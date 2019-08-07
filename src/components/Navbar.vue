cd225e6650dfc9564f96791388e1444974b9b9c7
kokochef <kakoty.kaustabh@gmail.com>
2019-08-07 14:19:03 +0530

    home page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">CodeSmash</router-link>
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/premium" class="button is-rounded"
+                >Premium</router-link
+              >
+              <router-link to="/login" class="button is-rounded"
+                >Log In</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.button {
+  background-color: transparent;
+  color: #0bfdd1;
+  border-color: transparent;
+}
+
+.button:hover,
+.button.is-hovered {
+  background-color: #0bfdd1;
+  color: #000;
+  border-color: transparent;
+}
+
+a:hover {
+  color: #0bfdd1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Navbar></Navbar>
+    <section class="hero is-fullheight-with-navbar">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-vcentered">
+            <div class="column is-half has-text-centered-mobile">
+              <p class="title">CodeSmash</p>
+              <p class="subtitle">
+                CodeSmash is the modern programming experience for educators and
+                competitive coders.
+              </p>
+              <router-link class="button is-rounded" to="/register"
+                >Create Account</router-link
+              >
+            </div>
+
+            <div class="column is-half">
+              <div class="container">
+                <figure class="image">
+                  <img src="@/assets/code-desk.svg" alt="Code Desk" />
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
-  name: "home",
+  name: "Home",
   components: {
-    HelloWorld
+    Navbar
   }
 };
 </script>
+
+<style scoped>
+.title {
+  color: #0bfdd1;
+}
+
+.subtitle {
+  color: #fff;
+}
+
+.button {
+  background-color: #0bfdd1;
+  color: #000;
+  border-color: transparent;
+}
+
+img {
+  max-width: 450px;
+  max-height: 450px;
+  margin: 0 auto;
+}
+</style>
